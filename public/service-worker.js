42f3cb1049b4d6c06c4e6435123d0ea599e1d200
gmunoz94 <g.munozii@icloud.com>
2021-09-11 18:29:50 -0500

    finalized service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/styles.css",
     "/index.js",
-    "/manifest.webmanifest"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -11,30 +10,49 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    )
-
-    self.skipWaiting();
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
 })
 
 self.addEventListener("activate", function(evt) {
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME]
     evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key != CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete)
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+})
 
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(DATA_CACHE_NAME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        })
+                    })
                 })
-            )
-        })
-    )
-
-    self.clients.claim();
+            })
+        )
+    }
 })
 
